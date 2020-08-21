a10dc6e0d2403fcace0adcbd2f0807c4fbda64a4
sbarex <sbarex@gmail.com>
2020-08-21 21:18:02 +0200

    Initial Commit

---
+++ ImageUtils/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageUtils
+//
+//  Created by Simone Baldissini on 21/08/2020.
+//  Copyright © 2020 sbarex. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
